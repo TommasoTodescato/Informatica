b6e86273f0c56a02a0f3b3857536d834004be8f5
TODESCATO TOMMASO <9698243@itisrossi.edu.it>
2024-03-14 09:33:00 +0100

    trie finito

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -5,20 +5,6 @@
 
         static void Main(string[] args)
         {
-            linked_list<int> list = new linked_list<int>();
-            list.push_back(5);
-            list.push_back(1254);
-            list.push_back(-345);
-            list.push_back(89);
-
-            list.remove(-345);
-
-            list.pop_back();
-            list.pop_front();
-
-            list.print();
-
-
             Console.WriteLine("----------------");
 
             trie t = new trie();
@@ -34,7 +20,6 @@
             Console.WriteLine(t.find_prefix("asd"));
 
             Console.WriteLine("----------------");
-
         }
     }
 }
--- DSA/trie.cs
+++ DSA/trie.cs
@@ -18,14 +18,14 @@
         public void add(string s)
         {
             trie_node tmp = root;
-            for (int i = 0; i < s.Count(); i++)
+            for (int i = 0; i < s.Length; i++)
             {
                 if (tmp.children.Keys.Contains(s[i]))
                     tmp = tmp.children[s[i]];
                 else
                 {
                     trie_node new_node = new trie_node();
-                    if ((s.Count() - 1) == i) new_node.word = true;
+                    if ((s.Length - 1) == i) new_node.word = true;
 
                     tmp.children.Add(s[i], new_node);
                     tmp = new_node;
@@ -36,12 +36,12 @@
         public bool find_word(string s)
         {
             trie_node tmp = root;
-            for (int i = 0; i < s.Count(); i++)
+            for (int i = 0; i < s.Length; i++)
             {
                 if (tmp.children.Keys.Contains(s[i]))
                 {
                     tmp = tmp.children[s[i]];
-                    if ((s.Count() - 1) == i && tmp.word == true) return true;
+                    if ((s.Length - 1) == i && tmp.word == true) return true;
                 }
                 else return false;
             }
@@ -51,7 +51,7 @@
         public bool find_prefix(string s)
         {
             trie_node tmp = root;
-            for (int i = 0; i < s.Count(); i++)
+            for (int i = 0; i < s.Length; i++)
             {
                 if (tmp.children.Keys.Contains(s[i]))
                     tmp = tmp.children[s[i]];
@@ -60,7 +60,5 @@
             }
             return true;
         }
-
-
     }
 }
