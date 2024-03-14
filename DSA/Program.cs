2a874a67b93e7bd8ffb1ee04ad2ca1cab0fcce9a
TODESCATO TOMMASO <9698243@itisrossi.edu.it>
2024-03-14 10:13:55 +0100

    asd

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -2,12 +2,19 @@
 {
     internal class Program
     {
-
+        
         static void Main(string[] args)
         {
+            var list = new linked_list<int>();
+            list.push_front(1);
+            list.push_front(2);
+            list.push_front(3);
+            list.remove(2);
+            list.print();
+
             Console.WriteLine("----------------");
 
-            trie t = new trie();
+            var t = new trie();
             t.init();
 
             t.add("ciao");
