390e44b11f351ba085dde3d2c0341c6b694da1ad
TODESCATO TOMMASO <9698243@itisrossi.edu.it>
2024-04-18 08:39:14 +0200

    graph

---
+++ ds/graph.cs
@@ -0,0 +1,49 @@
+﻿namespace ds
+{
+    internal class graph
+    {
+        int N;
+        List<List<int>> adj;
+
+        graph(int n)
+        {
+            N = n;
+            adj = new List<List<int>>();
+        }
+
+        void addEdge(int v, int w)
+        {
+            adj[v].Add(w);
+            adj[w].Add(v);
+        }
+
+        int[] BFS(int start)
+        {
+            bool[] visited = new bool[N];
+            int[] distance = new int[N];
+            Queue<int> q = new Queue<int>();
+            visited[start] = true;
+            distance[start] = 0;
+            q.Enqueue(start);
+            while (q.Count != 0)
+            {
+                int s = q.Dequeue();
+
+                foreach (int v in adj[s])
+                {
+                    if (visited[v]) continue;
+
+                    Console.WriteLine(v);
+
+                    visited[v] = true;
+                    distance[v] = distance[s] + 1;
+                    q.Enqueue(v);
+                }
+            }
+
+            return distance;
+        }
+
+
+    };
+}
