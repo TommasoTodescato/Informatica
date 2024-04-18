d07f9e81acbb39a627e992a6158d1eb63cd81d90
TODESCATO TOMMASO <9698243@itisrossi.edu.it>
2024-04-18 08:16:56 +0200

    ok

---
+++ ds/shangai.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace DSA
+{
+    internal class directed_graph
+    {
+        public int nodes { get; set; }
+        private Vector<Vector<int>> adj;
+
+        public directed_graph(int n)
+        {
+            nodes = n;
+            adj = new Vector<Vector<int>>();
+        }
+
+        public void add_node()
+        {
+        }
+
+        public void add_edge()
+        {
+        }
+
+        private void topo_sort()
+        {
+
+        }
+
+    }
+
+    internal class shangai
+    {
+
+    }
+}
